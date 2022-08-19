9ef6fb129d446af3f818384939e6ddd75d1f4b93
William Leung <gwilliamleung@gmail.com>
2022-08-19 19:47:40 -0400

    first commit starting with react npm

---
+++ src/Components/Main.js
@@ -0,0 +1,10 @@
+export default function Main(){
+    return(
+        <main>
+            <h1>William Leung</h1>
+            <h2>Aspiring Frontend Developer</h2>
+            <h3>gwilliamleung.com</h3>
+            <a href="google.com">Test</a>
+        </main>
+    )
+}

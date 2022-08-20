6b6b200346740158a4d090dd9899ec247abe94c8
William Leung <gwilliamleung@gmail.com>
2022-08-20 19:51:17 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Main from "./Components/Main"
 import Body from "./Components/Body"
+import Footer from "./Components/Footer"
+
 
 function App() {
   return (
     <div className="container">
       <Main />
       <Body />
+      <Footer />
     </div>
   );
 }

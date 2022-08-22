c011bbbad59baa02dbdcc3ba7fcf24cfee5029ba
William Leung <gwilliamleung@gmail.com>
2022-08-21 23:09:48 -0400

    picture added

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+export default function Header(){
+    return(
+        <header>
+            <img src="/Headshot.png"/>
+            <h1>William Leung</h1>
+            <h2>Aspiring Frontend Developer</h2>
+            <h3>gwilliamleung.com</h3>
+            <a href="google.com">Test</a>
+        </header>
+    )
+}
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,10 @@
 export default function Main(){
     return(
         <main>
-            <h1>William Leung</h1>
-            <h2>Aspiring Frontend Developer</h2>
-            <h3>gwilliamleung.com</h3>
-            <a href="google.com">Test</a>
+            <h2>About Me</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            <h2>About Me</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
         </main>
     )
 }

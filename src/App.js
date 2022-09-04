54e297458bd665e3e7260903ae885b255d510a24
William Leung <gwilliamleung@gmail.com>
2022-09-03 23:09:32 -0400

    updated components names bc of error, added pic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import Header from "./Components/Header"
 import Main from "./Components/Main"
-import Body from "./Components/Body"
 import Footer from "./Components/Footer"
+import './styles.css'
 
 
 function App() {
   return (
     <div className="container">
+      <Header />
       <Main />
-      <Body />
       <Footer />
     </div>
   );
